7a8ab33d9587622b819babb6535bc0ac3a678558
Francisco Sant'Anna <francisco.santanna@gmail.com>
2024-07-23 23:00:17 -0300

    ...

--- arduino-02/arduino-02.ino
+++ arduino-02/arduino-02.ino
@@ -1,9 +1,22 @@
+static char msg[23] = {
+  0x88,
+  'a', 'b', 'c', 'd', 'e', 'f',
+  'g',
+  //0b010000?0,         // inb=1, out=0
+  0b01000000,           // inb=1, out=0
+  //?, ?, ?, ?,         // 100 10 1 0.1
+  0, 0, 0, 0,
+  'h', 'i', 'j', 'k',
+  'l', 'm', 'n', 'o', 'p', 'q'
+};
+
 void setup () {
   Serial.begin(9600);
 }
 
 void loop () {
-  static int b = 0;
-  Serial.write('A' + (b++ % 63));
-  delay(2000);
+  static int i = 0;
+  Serial.write(msg[i]);
+  i = (i+1) % 23;
+  //delay(2000);
 }
