1c5429e0b4752e6ea15ed5e1f1cbb5c2e6d23b88
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-11-08 07:15:17 -0300

    - README, arduino/, minis/

---
+++ README.md
@@ -0,0 +1,9 @@
+- Atmos:
+    - `minis/01-radar.atm`: show random reads from `arduino/radar/`
+
+
+- Arduino:
+    - `arduino/radar`:
+        - random player
+        - random speed (50%: speed=0)
+
---
+++ arduino/radar/radar.ino
@@ -0,0 +1,31 @@
+static char msg[23] = {
+  0x88,
+  'a', 'b', 'c', 'd', 'e', 'f',
+  'g',
+  //0b010000?0,         // inb=1, out=0
+  0b01000010,           // inb=1, out=0
+  //?, ?, ?, ?,         // 100 10 1 0.1
+  '0', '5', '5', '0',
+  'h', 'i', 'j', 'k',
+  'l', 'm', 'n', 'o', 'p', 'q'
+};
+
+void setup () {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop () {
+  //delay(500);
+  msg[8]  = (random(0,2) == 0) ? 0b01000010 : 0b01000000;     // random L/R
+  if (random(0,2) == 0) {                                     // speed = 0
+    msg[10] = '0';
+    msg[11] = '0';
+  } else {                                                    // speed = [30,89]
+    msg[10] = random('3', '8');
+    msg[11] = random('0', '9');
+  }
+  for (int i=0; i<23; i++) {
+    Serial.write(msg[i]);
+  }
+}
